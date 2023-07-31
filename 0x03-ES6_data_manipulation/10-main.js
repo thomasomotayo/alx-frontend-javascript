533cc65dca9c34180bb18f25335925e63711a93a
thomasomotayo <thomasomotayo@gmail.com>
2023-07-31 18:58:52 +0000

    all task

---
+++ 0x03-ES6_data_manipulation/10-main.js
@@ -0,0 +1,7 @@
+import updateUniqueItems from "./10-update_uniq_items.js";
+import groceriesList from "./9-groceries_list.js";
+
+const map = groceriesList();
+console.log(map);
+
+updateUniqueItems(map)
