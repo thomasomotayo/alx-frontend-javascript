d43a2e9d96aa05a8a9d33a32f92ee6022fe92b2b
thomasomotayo <thomasomotayo@gmail.com>
2023-07-31 19:11:07 +0000

    task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,11 +5,4 @@ export default function cleanSet(set, startString) {
     if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
   });
   return result.slice(0, result.length - 1);
-}export default function cleanSet(set, startString) {
-  let result = '';
-  if (!startString || !startString.length) return result;
-  set.forEach((i) => {
-    if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
-  });
-  return result.slice(0, result.length - 1);
 }
